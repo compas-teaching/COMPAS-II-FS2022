99b5b8de07b349b11a8b09015d384cbc3975c379
wenjun-dbt <wenjun@dbt-ethz.ch>
2022-03-03 17:48:15 +0100

    Solved assignment

--- lecture_02/assignment_01/wenjun_liu/assignment_01.py
+++ lecture_02/assignment_01/wenjun_liu/assignment_01.py
@@ -3,28 +3,41 @@
 from compas.geometry import Box
 from compas.geometry import Frame
 from compas.geometry import Projection
+from compas.geometry import Plane
 from compas.artists import Artist
 from compas.datastructures import Mesh
 
 # Define a Frame, which is not in the origin and a bit tilted to the world frame
-# frame =
+frame =Frame([1,9,7],[-0.5,0.5,0.5],[0.5,0.4,0.2])
 
 # Create a Box with that frame
-# box = ...
+box = Box(frame, 2, 2, 2)
 
 # Create a Projection (can be orthogonal, parallel or perspective)
-# P = Projection.from_...
+plane = Plane([0,0,0],[0,0,1])
+P1 = Projection.from_plane(plane)
+P2 = Projection.from_plane_and_direction(plane,[0,1,1])
+P3 = Projection.from_plane_and_point(plane, [50,50,50])
 
 # Create a Mesh from the Box
 mesh = Mesh.from_shape(box)
 
 # Apply the Projection onto the mesh
-# mesh_projected = ...
+mesh_projected_1 = mesh.transformed(P1)
+mesh_projected_2 = mesh.transformed(P2)
+mesh_projected_3 = mesh.transformed(P3)
 
 # Create artists
 artist1 = Artist(box)
-artist2 = Artist(mesh_projected)
+artist2 = Artist(mesh_projected_1)
+artist3 = Artist(mesh_projected_2)
+artist4 = Artist(mesh_projected_3)
 
 # Draw
 artist1.draw()
-artist2.draw_edges(color="#00ff00")
+artist2.draw_edges(color=[0, 255, 255])
+artist3.draw_edges(color=[255, 0, 255])
+artist4.draw_edges(color=[255, 255, 0])
+
+
+
