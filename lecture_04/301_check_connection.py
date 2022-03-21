da48f6b946e0490784b0683186ded20e88931e82
yinxiao <yinan.xiao@tu-braunschweig.de>
2022-03-21 14:25:46 +0100

    assignment_03 python and JSON file

--- lecture_04/301_check_connection.py
+++ lecture_04/301_check_connection.py
@@ -1,4 +1,16 @@
 from compas_fab.backends import RosClient
 
+# With context manager
 with RosClient("localhost") as client:
     print("Connected:", client.is_connected)
+
+# Without context manager
+# client = RosClient("localhost")
+# try:
+#     client.run()
+#     print("Connected:", client.is_connected)
+# except:
+#     pass
+# finally:
+#     if client:
+#         client.close()
