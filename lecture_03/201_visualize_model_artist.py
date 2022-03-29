5762cd198dcb582817062b104790af4ec06d6669
ZHAOHANBING1024 <zhao@dbt-ethz.ch>
2022-03-30 00:40:27 +0200

    assignment-03

--- lecture_03/201_visualize_model_artist.py
+++ lecture_03/201_visualize_model_artist.py
@@ -1,9 +1,10 @@
 from compas.artists import Artist
 from compas.robots import RobotModel
 
-model = RobotModel.from_urdf_file("models/01_myfirst.urdf")
-
+model = RobotModel.from_urdf_file("models/06_with_colors.urdf")
+print model
 artist = Artist(model, layer="Robot")
 artist.clear_layer()
 artist.draw_visual()
 artist.redraw()
+
