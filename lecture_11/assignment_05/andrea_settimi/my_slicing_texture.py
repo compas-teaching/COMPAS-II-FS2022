0411deb7785d18a60c461333722472c5c1c6563a
Andrea Settimi <andreasettimi39@gmail.com>
2022-06-01 09:36:54 +0200

    new slicing textures

--- lecture_11/assignment_05/andrea_settimi/my_slicing_texture.py
+++ lecture_11/assignment_05/andrea_settimi/my_slicing_texture.py
@@ -5,17 +5,20 @@ from compas.geometry import scale_vector, add_vectors
 
 def create_overhang_texture(slicer, overhang_distance):
     for i,layer in enumerate(slicer.layers):
-        if (i%4==0):
+        if (i%2==0):
             for j,path in enumerate(layer.paths):
                 new_path =[]
                 for k,point in enumerate(path.points):
-                    if (k%2 ==0) and (k%4==0):
+                    if (k%2 ==0) and (k%6==0):
                         normal = get_normal_of_path_on_xy_plane(k , point, path , mesh =None)
-                        normal_scaled = scale_vector(normal, -1*overhang_distance)
+                        normal_scaled = scale_vector(normal, -1.5*overhang_distance)
                         new_pt = add_vectors(point, normal_scaled)
                         new_path.append(Point(new_pt[0], new_pt[1], new_pt[2]))
+                    if (k%3 ==0) :
+                        normal = get_normal_of_path_on_xy_plane(k , point, path , mesh =None)
+                        normal_scaled = scale_vector(normal, 1*overhang_distance)
+                        new_pt2 = add_vectors(point, normal_scaled)
+                        new_path.append(Point(new_pt2[0], new_pt2[1], new_pt2[2]))
                     else:
                         new_path.append(point)
-
             layer.paths[j]= Path(new_path, is_closed= path.is_closed)
-
